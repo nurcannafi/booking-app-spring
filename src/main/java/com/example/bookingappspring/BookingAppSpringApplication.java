24b77d5057fb6f05de47d542c631f01b8830307a
nurcan <nurcannafii@outlook.com>
2024-12-25 20:55:26 +0400

    Initial commit

---
+++ src/main/java/com/example/bookingappspring/BookingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookingappspring/BookingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
